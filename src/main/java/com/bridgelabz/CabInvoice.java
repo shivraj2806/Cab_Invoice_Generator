68c87b524bd0c6d75dc6f659c8250c0ef8aece70
shivraj2806 <shivjadhav2806@gmail.com>
2021-10-25 22:35:10 +0530

    Added: find total fare

--- src/main/java/com/bridgelabz/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoice.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
 public class CabInvoice {
-    public static void main(String[] args) {
-        System.out.println("welcome message");
-    }
-    }
+        double COST_PER_KILOMETER=10;
+        int COST_PER_MINUTE=1;
+        double MINIMUM_FARE=5;
 
+        public double totalFare(double kilometer,int minute){
+            double fare=(COST_PER_KILOMETER*kilometer + COST_PER_MINUTE * minute);
+            if(fare<5){
+                return  MINIMUM_FARE;
+            }
+            return fare;
+        }
+    }
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,2 +1,23 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CabInvoiceTest {
+
+        CabInvoice invoice=new CabInvoice();
+
+        @Test
+        public void whenEnterValue_returnTotalFare(){
+
+            double res=invoice.totalFare(0.8,2);
+            Assertions.assertEquals(10.0,res,0.0);
+        }
+
+        @Test
+        public void whenMinimumDistance_returnMinimumFare(){
+            double result=invoice.totalFare(0.2,2);
+            Assertions.assertEquals(5,result,0.0);
+        }
+    }
+
